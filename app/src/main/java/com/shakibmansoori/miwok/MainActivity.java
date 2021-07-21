3d6ae15a94400c8bc503b66741d41a62a1fc52a9
Shakib Hussain <shakibraza436@gmail.com>
2021-07-21 21:58:38 +0530

    Set On Click Listener On All Activities

--- app/src/main/java/com/shakibmansoori/miwok/MainActivity.java
+++ app/src/main/java/com/shakibmansoori/miwok/MainActivity.java
@@ -1,6 +1,8 @@
 package com.shakibmansoori.miwok;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,12 +14,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        NumbersOnClickListeners numbersOnClickListeners = new NumbersOnClickListeners();
+        TextView numbersTv, familyTv, colorsTv, phrasesTv;
 
-        TextView textView = findViewById(R.id.numbersActiviyt);
+        numbersTv = findViewById(R.id.numbersActiviyt);
+        familyTv = findViewById(R.id.familyActiviyt);
+        colorsTv = findViewById(R.id.colorsActiviyt);
+        phrasesTv = findViewById(R.id.phrasesActiviyt);
 
-        textView.setOnClickListener(numbersOnClickListeners);
-
+        numbersTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), NumbersActivity.class);
+                startActivity(intent);
+            }
+        });
+        familyTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), FamilyActivity.class);
+                startActivity(intent);
+            }
+        });
+        colorsTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ColorsActivity.class);
+                startActivity(intent);
+            }
+        });
+        phrasesTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), PhrasesActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
